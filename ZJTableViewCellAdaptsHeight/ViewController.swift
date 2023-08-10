3a637206186bb36426e0de70b8cc9e4b6ebdd14c
jinxiang.zhang <jinxiang.zhang@cos-capital.com>
2023-08-10 16:51:22 +0800

    TableViewCell缓存高度实现

--- ZJTableViewCellAdaptsHeight/ViewController.swift
+++ ZJTableViewCellAdaptsHeight/ViewController.swift
@@ -13,12 +13,80 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         setup()
     }
+    
+    //MARK: lazy
+    private lazy var tableView: UITableView = {
+        let table = UITableView()
+        table.backgroundColor = UIColor.white
+        table.delegate = self
+        table.dataSource = self
+        table.tableFooterView = UIView()
+        return table
+    }()
+    
+    private lazy var dataArray: NSArray? = {
+        guard let path = Bundle.main.path(forResource: "data", ofType: "plist") else {
+            return NSArray()
+        }
+        return NSArray(contentsOfFile: path)
+    }()
+    
+    private lazy var cellHeightDic: Dictionary = Dictionary<IndexPath, CGFloat>()
 
     //MARK: setup
     private func setup() {
-        navigationItem.title = "Cell自适应高度"
-        view.backgroundColor = UIColor.white
+        title = "Cell缓存高度"
+        view.backgroundColor = .white
+        
+        view.addSubview(tableView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addConstraint(NSLayoutConstraint(item: tableView, attribute: .left, relatedBy: .equal, toItem: view, attribute: .left, multiplier: 1, constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: tableView, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1, constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: tableView, attribute: .right, relatedBy: .equal, toItem: view, attribute: .right, multiplier: 1, constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: tableView, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: 0))
+        
+        tableView.reloadData()
     }
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray!.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        let titleLabel = UILabel()
+        titleLabel.textColor = .black
+        titleLabel.font = .systemFont(ofSize: 15)
+        titleLabel.numberOfLines = 0
+        cell.contentView.addSubview(titleLabel)
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        cell.contentView.addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .left, relatedBy: .equal, toItem: cell.contentView, attribute: .left, multiplier: 1, constant: 15))
+        cell.contentView.addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .top, relatedBy: .equal, toItem: cell.contentView, attribute: .top, multiplier: 1, constant: 10))
+        cell.contentView.addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .right, relatedBy: .equal, toItem: cell.contentView, attribute: .right, multiplier: 1, constant: -10))
+        cell.contentView.addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .bottom, relatedBy: .equal, toItem: cell.contentView, attribute: .bottom, multiplier: 1, constant: -10))
+        
+        titleLabel.text = dataArray![indexPath.row] as? String
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cellHeightDic.updateValue(cell.bounds.size.height, forKey: indexPath)
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let height = cellHeightDic[indexPath] else {
+            return 44
+        }
+        return height
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
